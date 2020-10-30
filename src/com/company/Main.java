0ee2f0ed37861beae98439ea31d33b6df7ee0b9e
dianabaydulina <73581596+dianabaydulina@users.noreply.github.com>
2020-10-30 15:31:15 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int mas1[ ] = {18, 19, 20};
+	String mas2[ ] = {"Ivan", "Vlad", "Vera"};
+	for (int i = 0; i < mas1.length; i++)
+    {
+        System.out.println(mas2[i] + " Is " + mas1[i]);
+    }
+    }
+}
+/*package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int mas1[ ] = {"Ivan", "Vlad", "Vera"};
+	string mas2[ ] = {18, 19, 20}
+	for (int j = 0; j < lenght(mas1); i++)
+    {
+        System.out.println(mas1[j] + mas2[j]);
+    }
+    }
+}*/
